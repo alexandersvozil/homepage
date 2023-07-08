39ff21fb39a1a334db9c98677f1d638dd76880ca
Alexander Svozil <alexander.svozil@gmail.com>
2023-07-08 17:09:42 +0200

    updated width for mobile devices

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -6,14 +6,14 @@ import Date from "../../components/date";
 export default function Post({ postData }) {
   console.log(postData.contentHtml);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="min-h-screen py-2 px-4 sm:px-6 md:px-8 lg:px-10">
       <Head>
         <title>{postData.title}</title>
       </Head>
       <article>
         <h1 className="text-4xl font-bold mb-4">{postData.title}</h1>
         <div
-          className="prose"
+          className="prose prose-xl"
           dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
         />
       </article>
